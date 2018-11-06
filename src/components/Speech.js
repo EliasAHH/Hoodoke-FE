c8eae19fbc37c6cae3e6da4ead95a616a288ddd2
Flatiron School <flatironschool@Flatirons-MacBook-Air-6.local>
2018-11-06 17:55:33 -0500

    fixed speech component

--- src/components/Speech.js
+++ src/components/Speech.js
@@ -3,6 +3,10 @@ import SpeechRecognition from 'react-speech-recognition'
 
 
 class Speech extends Component {
+  state = {
+    toggle:false
+  }
+
   render() {
     const { transcript, resetTranscript, browserSupportsSpeechRecognition, startListening, stopListening, finalTranscript } = this.props
 
@@ -11,15 +15,30 @@ class Speech extends Component {
     }
 
     const handleClick = () => {
-      startListening()
-      console.log(finalTranscript)
+      let listening = !this.state.toggle
+      this.setState({
+      toggle:listening
+    },listenSpeech)
+    }
+
+    const listenSpeech = () => {
+      if(this.state.toggle === true ) {
+        startListening()
+        console.log("in the true block", finalTranscript)
+      }
+      else{
+        stopListening()
+        console.log("in the else block", finalTranscript)
+      }
 
     }
 
     const componentWillUnmount = () => {
       stopListening()
+      resetTranscript()
       console.log(finalTranscript)
     }
+    console.log(finalTranscript)
 
     return (
       <div>
